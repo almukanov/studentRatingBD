8db19d1f96b41a92649fbe3e1b5ca8bf645b4116
Ruslan <ruslan@almukanov.ru>
2022-02-11 13:50:18 +0500

    version 2.0

--- src/main/java/ru/almukanov/Main.java
+++ src/main/java/ru/almukanov/Main.java
@@ -1,10 +1,28 @@
 package ru.almukanov;
 
 
-public class Main {
+import org.hibernate.Session;
 
-    public static void main(String[] args) {
-        Processing.process();
+import ru.almukanov.classes.Rating;
+import ru.almukanov.classes.Students;
+import ru.almukanov.services.RateService;
+import ru.almukanov.services.StudentService;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+StudentService studentService = new StudentService();
+        RateService rateService = new RateService();
+
+
+        Rating rate = new Rating(studentService.findAllStudents().get(1),6.18);
+
+        rateService.updateRate(rate);
+
 
     }
+
+
+
 }

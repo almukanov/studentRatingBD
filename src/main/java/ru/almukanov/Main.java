5a03d83fa2514586fe5f05440864bb82bd7fca94
ruslan@almukanov.ru <RsLn545763Gi!>
2022-02-19 12:54:27 +0500

    2.5.0 Beta - log4j

--- src/main/java/ru/almukanov/Main.java
+++ src/main/java/ru/almukanov/Main.java
@@ -1,10 +1,14 @@
 package ru.almukanov;
 
 
+import org.apache.log4j.PropertyConfigurator;
 import org.hibernate.Session;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.almukanov.classes.Rating;
 import ru.almukanov.classes.Students;
+import ru.almukanov.dao.RateDaoImpl;
 import ru.almukanov.services.RateService;
 import ru.almukanov.services.StudentService;
 
@@ -12,6 +16,9 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+        PropertyConfigurator.configure("C:\\java\\studentRatingBD\\src\\main\\resources\\log4j.properties");
+
+
       Processing.process();
 
 
--- src/main/java/ru/almukanov/dao/RateDaoImpl.java
+++ src/main/java/ru/almukanov/dao/RateDaoImpl.java
@@ -2,14 +2,18 @@ package ru.almukanov.dao;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.almukanov.HibernateUtil;
 import ru.almukanov.classes.Grade;
 import ru.almukanov.classes.Rating;
 import ru.almukanov.classes.Students;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class RateDaoImpl implements RateDao {
+    Logger logger = LoggerFactory.getLogger(RateDaoImpl.class);
 
     public double select(int id) {
        return ((double) HibernateUtil.getSessionFactory().openSession().createQuery("SELECT rating FROM Rating where student =" + id).list().get(0));
@@ -23,14 +27,18 @@ public class RateDaoImpl implements RateDao {
         session.save(rate);
         tx1.commit();
         session.close();
+        logger.info("TEST");
     }
 
     public void update(Rating rate) {
+        LocalDateTime dt1 = LocalDateTime.now();
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction tx1 = session.beginTransaction();
             session.update(rate);
             tx1.commit();
             session.close();
+        LocalDateTime dt2 = LocalDateTime.now();
+        logger.info("TEST2 {} {}",dt1, dt2);
 
     }
 

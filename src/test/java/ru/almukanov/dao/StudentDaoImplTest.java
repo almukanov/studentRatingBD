875a37ee4b97fa414a1da4181144c77d987c34cd
ruslan@almukanov.ru <RsLn545763Gi!>
2022-02-20 17:07:29 +0500

    2.5.2 - Unit tests

--- src/test/java/ru/almukanov/dao/StudentDaoImplTest.java
+++ src/test/java/ru/almukanov/dao/StudentDaoImplTest.java
@@ -6,10 +6,20 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import ru.almukanov.HibernateUtil;
+import ru.almukanov.Processing;
+import ru.almukanov.classes.Grade;
 import ru.almukanov.classes.Students;
+import ru.almukanov.services.GradeService;
+import ru.almukanov.services.RateService;
+import ru.almukanov.services.StudentService;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Scanner;
 
+import static java.lang.System.in;
+import static java.lang.System.out;
 import static org.junit.Assert.*;
 
 public class StudentDaoImplTest {
@@ -25,5 +35,15 @@ public class StudentDaoImplTest {
         Assert.assertEquals(l.get(0),"Иван");
     }
 
+    @Test
+    public void NotRightIndex(){
+try {
+  Grade s = (Grade) HibernateUtil.getSessionFactory().openSession().createQuery("FROM Grade WHERE id =" + 100).getSingleResult();
+    Assert.fail();
+}catch (Exception ex){
+    ex.printStackTrace();
+}
+
+    }
 
 }
